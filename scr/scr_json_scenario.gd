84aa795f893aadd895ce098344141ec0559f97e2
Leifer <leifer.kopf@gmail.com>
2017-09-08 21:20:36 +0200

    Ajustements
    Ajustements divers

--- scr/scr_json_scenario.gd
+++ scr/scr_json_scenario.gd
@@ -2,7 +2,7 @@
 # Script par LEIFER KOPF // leifer.kopf@gmail.com
 # Scénario par NOOTILUS //
 # Disclaimer : L'ensemble du contenu de ce document est la propriété de GalaaDScript, il ne peut être utilisé, même partiellement sans accord préalable de GalaaDScript (Filliale du groupe AE-Com).
-# version : 0.22
+# version : 0.26
 
 extends Control
 
@@ -19,13 +19,13 @@ func _ready():
 func start():
 	print("start")
 	
-	# Ouverture / Parse / Fermeture du fichier JSON
+# Ouverture / Parse / Fermeture du fichier JSON
 	var file = File.new()
 	file.open("res://json/credit.json", file.READ)
 	dict.parse_json(file.get_as_text())
 	file.close()
 	
-	# Timer
+# Timer
 	time_delay = dict._Dialogues[currentDial].time
 	timer = get_node("Timer")
 	timer.set_wait_time(time_delay)
@@ -35,8 +35,8 @@ func _on_Timer_timeout():
 	print("timer")
 	write = true
 
-	# Gestion des dialogues de ref 1 [DIALOGUES]
-	while currentDial in dict._Dialogues and dict._Dialogues[currentDial].ref == 1 and write == true :
+# Gestion des dialogues de ref 1 [DIALOGUES]
+	while currentDial in dict._Dialogues and dict._Dialogues[currentDial].ref == 1 and write :
 		print("Dialogues")
 		get_node("Dialogues").push_align(0)
 		get_node("Dialogues").add_text(str("\n [",currentDial,"] : ",dict._Dialogues[currentDial].content))
@@ -44,7 +44,7 @@ func _on_Timer_timeout():
 		write = false
 		start()
 	
-	# Gestion des dialogues de ref 2 [REPONSES CHOIX MULTIPLES]
+# Gestion des dialogues de ref 2 [REPONSES CHOIX MULTIPLES]
 	if dict._Dialogues[currentDial].ref == 2 :
 		print("Réponses choix multiples")
 		for i in range(dict._Dialogues[currentDial].content.size()):
@@ -54,14 +54,15 @@ func _on_Timer_timeout():
 		timer.stop()
 	
 	
-	# Gestion des dialogues de ref 3 [REPONSES VIA TEXTE PRECIS]
+# Gestion des dialogues de ref 3 [REPONSES VIA TEXTE PRECIS]
 	if dict._Dialogues[currentDial].ref == 3:
 		print("Réponses via texte")
 		get_node("TextEdit").show()
 		get_node("TextEdit").clear()
 		timer.stop()
-	# Gestion des boutons de choix multipes
-	# BOUTON 0
+	
+# Gestion des boutons de choix multipes
+# BOUTON 0
 func _on_Bouton0_pressed():
 	print("Bouton 0")
 	get_node("Dialogues").push_align(2)
@@ -70,7 +71,7 @@ func _on_Bouton0_pressed():
 	clean()
 	start()
 	
-	# BOUTON 1
+# BOUTON 1
 func _on_Bouton1_pressed():
 	print("Bouton 1")
 	get_node("Dialogues").push_align(2)
@@ -79,7 +80,7 @@ func _on_Bouton1_pressed():
 	clean()
 	start()
 	
-	# BOUTON 2
+# BOUTON 2
 func _on_Bouton2_pressed():
 	print("Bouton 2")
 	get_node("Dialogues").push_align(2)
@@ -88,7 +89,7 @@ func _on_Bouton2_pressed():
 	clean()
 	start()
 	
-	# BOUTON 3
+# BOUTON 3
 func _on_Bouton3_pressed():
 	print("Bouton 3")
 	get_node("Dialogues").push_align(2)
@@ -97,7 +98,7 @@ func _on_Bouton3_pressed():
 	clean()
 	start()
 	
-	# Nettoyage des boutons inutiles
+# Nettoyage des boutons inutiles
 func clean():
 	for i in range(4):
 		print("Suppression bouton")
@@ -105,7 +106,7 @@ func clean():
 		get_node(str("Bouton",i)).set_ignore_mouse(true)
 		get_node(str("Bouton",i)).set_flat(true)
 
-
+# Boite de dialogue pour écrire la réponse demandée.
 func _on_TextEdit_text_entered( text ):
 	if get_node("TextEdit").get_text() == dict._Dialogues[currentDial].content[0]:
 		get_node("Dialogues").push_align(2)
