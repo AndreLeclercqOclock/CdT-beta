aa9d9fb4b5b0a6bda8e51b8f860049a693f458ef
Leifer <leifer.kopf@gmail.com>
2017-09-20 18:39:04 +0200

    Correction Save/Load Auto
    Correction du bug qui faisait buggé la réécriture du fichier de sauvegarde au chargement. Un nouveau bug est apparu si on ferme le jeu juste après avoir cliqué sur une réponse le jeu plantera au prochaine lancement.

--- scr/scr_json_scenario.gd
+++ scr/scr_json_scenario.gd
@@ -28,6 +28,7 @@ var data = null
 var dataDial = null
 var dataRep = null
 var firstDial = null
+var temp = null
 
 
 # Initialisation des bases du script
@@ -75,17 +76,34 @@ func _ready():
 	if stateSave == 1:
 		if save._Save.dial.size() > 1:
 			print("Chargement de la sauvegarde")
-			dataDial = save._Save.dial
-			dataRep = save._Save.rep
+			print("Réécriture de la sauvegarde")
+			print("Réécriture Dialogues dans le JSON")
+			for i in range(save._Save.dial.size()):
+				temp = str(save._Save.dial[i])
+				if dataDial != null:
+					dataDial = str(dataDial,'","',temp)
+				elif dataDial == null:
+					dataDial = str(temp)
+			print("Réécriture Réponses dans le JSON")
+			for i in range(save._Save.rep.size()):
+				temp = str(save._Save.rep[i])
+				if dataRep != null:
+					dataRep = str(dataRep,',',temp)
+				elif dataRep == null:
+					dataRep = str(temp)
+
+			print("Ecriture des textes")
 			for i in range(save._Save.dial.size()):
 				currentDial = save._Save.dial[i]
 				get_node("vbox/Mid/Patch/Dialogues").set_scroll_follow(true)
 				if dict._Dialogues[currentDial].ref == 1:
+					print("Ecriture du Dialogue")
 					get_node("vbox/Mid/Patch/Dialogues").push_align(0)
 					for y in range(dict._Dialogues[currentDial].content.size()):
 						get_node("vbox/Mid/Patch/Dialogues").newline()
 						get_node("vbox/Mid/Patch/Dialogues").add_text(str(dict._Dialogues[currentDial].content[y]))
 				elif dict._Dialogues[currentDial].ref == 2:
+					print("Ecriture de la réponse")
 					get_node("vbox/Mid/Patch/Dialogues").push_align(2)
 					get_node("vbox/Mid/Patch/Dialogues").newline()
 					get_node("vbox/Mid/Patch/Dialogues").add_text(str("Moi : ",dict._Dialogues[currentDial].content[i]))
